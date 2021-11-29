e970f544758ed1557321a06781800a53d1dc8d0f
mark <mark@discoverglobal.co.uk>
2021-11-29 13:47:34 +0000

    people that matter fields

--- wp-content/themes/twentytwentyone-child-theme_01/template-parts/people-that-matter2.php
+++ wp-content/themes/twentytwentyone-child-theme_01/template-parts/people-that-matter2.php
@@ -25,46 +25,6 @@
                     // no rows found
                 endif;?>
 
-
-<!--            <div class="col-4 people-col-container" style="">-->
-<!---->
-<!--                <div class=" people-col-new">-->
-<!---->
-<!--                </div>-->
-<!--                <h4 class="people-that-matter-title">Clients</h4>-->
-<!--                <p class="tag-line-new">You're the most important person-->
-<!--                    we can ever meet, click here to find-->
-<!--                    out what your LB journey-->
-<!--                    could look like.</p>-->
-<!--                <button class="button-orange scaled-button">discover more</button>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="col-4 people-col-container" style="">-->
-<!--                <div class="people-col-new" style="background-image: url('/wp-content/uploads/2021/10/clients.jpg');">-->
-<!---->
-<!--                </div>-->
-<!--                <h4 class="people-that-matter-title">Our People</h4>-->
-<!--                <p class="tag-line-new">Highly qualified, professional,-->
-<!--                    and always ready to answer any query,-->
-<!--                    our staff are great people-->
-<!--                    who are keen to help.</p>-->
-<!--                <a href="/our-team"><button class="button-orange scaled-button">meet the team</button></a>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="col-4 people-col-container" style="">-->
-<!--                <div class="people-col-new" style="background-image: url('/wp-content/uploads/2021/11/community2.jpg');  background-position: top -100px;">-->
-<!--                    -->
-<!--                </div>-->
-<!--                <h4 class="people-that-matter-title">Community</h4>-->
-<!--                <p class="tag-line-new">We believe in making an impact.-->
-<!--                    Our local community is no different,-->
-<!--                    see the work we do in our-->
-<!--                    local communities.</p>-->
-<!--                <a href="/our-team"><button class="button-orange scaled-button">join us</button></a>-->
-<!--            </div>-->
-
-
-
         </div>
     </div>
 </div>
